Fix for 6D rotation conversion

benchmark.py
@@ -11,8 +11,8 @@ from src.dataset import GraphSkeletonDataset
 from src.model import SkeletalMotionInterpolator
 from src.utils.metrics import (geodesic_rotation_loss, calculate_l2p, calculate_l2q, calculate_npss, 
         compute_smoothness_loss, foot_contact_loss) 
-from src.utils.bvh import forward_kinematics_positions_batch, compute_foot_contact, foot_skating_loss
-from src.utils.rotation import rot_6d_to_euler_zyx, euler_zyx_to_rot_6d_but_correct_this_time
+from src.utils.bvh import forward_kinematics_positions_batch, compute_foot_contact, foot_skating_loss, get_joint_indices_by_name
+from src.utils.rotation import rot_6d_to_euler_zyx
 
 
 # Config
@@ -80,35 +80,16 @@ def run_benchmark(model, loader, J, F_target, offsets, parent_indices, root_mean
             out = model(batch)
 
             # Rotations
-            rot_pred = out["rot"] # [B * J, F_target * 6]
-            rot_tgt = batch.y # [B * J, F_target * 6]
-
-            # All models to this time were trained with wrong 6D representation, 
-            # but Euler angles were correct after converting back,
-            # fix for training and prediction will be in next version
-
-            rot_pred = rot_pred.view(batch.num_graphs, J, F_target, 6).permute(0, 2, 1, 3) # 6D are wrong
-            rot_tgt = rot_tgt.view(batch.num_graphs, J, F_target, 6).permute(0, 2, 1, 3)
-            
-            rot_euler_pred = rot_6d_to_euler_zyx(rot_pred) # eulers are correct
-            rot_tgt_euler = rot_6d_to_euler_zyx(rot_tgt)
-
-            rot_pred = euler_zyx_to_rot_6d_but_correct_this_time(rot_euler_pred) # those 6D are correct
-            rot_tgt = euler_zyx_to_rot_6d_but_correct_this_time(rot_tgt_euler)
-
-            rot_pred = torch.tensor(rot_pred, dtype=torch.float32).to(device)
-            rot_tgt = torch.tensor(rot_tgt, dtype=torch.float32).to(device)
-
-            rot_pred_flat = rot_pred.permute(0, 2, 1, 3).reshape(batch.num_graphs * J, F_target * 6)
-            rot_tgt_flat = rot_tgt.permute(0, 2, 1, 3).reshape(batch.num_graphs * J, F_target * 6)
-
-            rot_geo_1_sum += rot_geo_1(rot_pred_flat, rot_tgt_flat).item()
-            rot_geo_2_sum += rot_geo_2(rot_pred_flat, rot_tgt_flat).item()
+            rot_pred = out["rot"] 
+         
+            rot_geo_1_sum += rot_geo_1(rot_pred, batch.y).item()
+            rot_geo_2_sum += rot_geo_2(rot_pred, batch.y).item()
 
             # L2Q
-            l2q_sum += calculate_l2q(rot_pred_flat, rot_tgt_flat, reduction='sum').item()
+            l2q_sum += calculate_l2q(rot_pred, batch.y, reduction='sum').item()
             
             # Forward kinematics
+            rot_pred = rot_pred.view(batch.num_graphs, J, F_target, 6).permute(0, 2, 1, 3) 
             root_pos_pred = out['root_pos']
             root_pos_pred = root_pos_pred.view(batch.num_graphs, F_target, 3)
             root_pos_pred = root_pos_pred * root_std + root_mean
@@ -130,20 +111,13 @@ def run_benchmark(model, loader, J, F_target, offsets, parent_indices, root_mean
             root_pos_l2_sum += l2(root_pos_pred_absolute, root_pos_tgt_absolute).item()
 
             # All positions
-            fk_pos_tgt = forward_kinematics_positions_batch( 
-                offsets=offsets,
-                parent_indices=parent_indices,
-                root_pos=root_pos_tgt_absolute,
-                rot_6d=rot_tgt
-            ) # [B, F_target, J, 3]
-
-            fk_pos_tgt_flat = fk_pos_tgt.view(batch.num_graphs, -1)
+            fk_pos_tgt_flat = fk_pos_tgt_reshaped.view(batch.num_graphs, -1)
             fk_pos_pred_flat = fk_pos_pred.view(batch.num_graphs, -1)
             all_pos_l1_sum += l1(fk_pos_pred_flat, fk_pos_tgt_flat).item()
             all_pos_l2_sum += l2(fk_pos_pred_flat, fk_pos_tgt_flat).item()
 
             # L2P
-            l2p_sum += calculate_l2p(fk_pos_pred, fk_pos_tgt, reduction='sum').item()
+            l2p_sum += calculate_l2p(fk_pos_pred, fk_pos_tgt_reshaped, reduction='sum').item()
 
             # NPSS
             fk_pos_pred_npss = fk_pos_pred.permute(0, 2, 3, 1).reshape(batch.num_graphs, J * 3, F_target)
@@ -157,7 +131,7 @@ def run_benchmark(model, loader, J, F_target, offsets, parent_indices, root_mean
 
             # Foot contact
             foot_contact_tgt = compute_foot_contact(
-                fk_pos=fk_pos_tgt,
+                fk_pos=fk_pos_tgt_reshaped,
                 foot_joint_indices=foot_joint_indices,
                 contact_height_eps=foot_height_eps,
                 contact_velocity_eps=foot_velocity_eps
@@ -244,10 +218,7 @@ test_dataset = GraphSkeletonDataset(
     context_len_pre=config["context_len_pre"],
     context_len_post=config["context_len_post"],
     target_len=config["target_len"],
-    step=config["step"],
-    foot_joint_names=constants["benchmark_foot_joint_names"],
-    foot_height_eps=constants["benchmark_foot_height_eps"],
-    foot_velocity_eps=constants["benchmark_foot_velocity_eps"]
+    step=config["step"]
 )
 print(f"Dataset ready with {len(test_dataset)} samples")
 
@@ -281,6 +252,10 @@ print(f"Loaded root stats from: {root_stats_path}")
 root_mean = torch.tensor(stats["mean"], dtype=torch.float32).to(device).view(1, 1, 3)
 root_std = torch.tensor(stats["std"], dtype=torch.float32).to(device).view(1, 1, 3)
 
+joint_names = test_dataset.joint_names
+foot_joint_names = constants["benchmark_foot_joint_names"]
+foot_joint_indices = get_joint_indices_by_name(all_joint_names=joint_names, target_joint_names=foot_joint_names)
+
 results = run_benchmark(
     model=model,
     loader=test_loader,
@@ -290,7 +265,7 @@ results = run_benchmark(
     parent_indices=test_dataset.parent_indices.to(device),
     root_mean=root_mean,
     root_std=root_std,
-    foot_joint_indices=test_dataset.foot_joint_indices.to(device),
+    foot_joint_indices=foot_joint_indices,
     foot_height_eps=constants["benchmark_foot_height_eps"],
     foot_velocity_eps=constants["benchmark_foot_velocity_eps"]
 )

src/dataset.py
@@ -9,10 +9,8 @@ from .utils.bvh import (
 ) 
 
 
-
 class GraphSkeletonDataset(Dataset):
-    def __init__(self, root_dir, context_len_pre, context_len_post, target_len, step, foot_joint_names,
-                foot_height_eps, foot_velocity_eps):
+    def __init__(self, root_dir, context_len_pre, context_len_post, target_len, step):
 
         super().__init__()
         self.context_len_pre = context_len_pre
@@ -41,16 +39,6 @@ class GraphSkeletonDataset(Dataset):
                     root_pos=root_pos, 
                     rot_6d=rot_6d
                 )
-                foot_joint_indices = get_joint_indices_by_name(
-                    all_joint_names=joint_names,
-                    target_joint_names=foot_joint_names
-                )
-                foot_contact = compute_foot_contact(
-                    fk_pos=fk_pos,
-                    foot_joint_indices=foot_joint_indices,
-                    contact_height_eps=foot_height_eps,
-                    contact_velocity_eps=foot_velocity_eps
-                )
                 root_pos_deltas = compute_root_deltas(root_pos)
                 data = {
                     'root_pos_deltas': root_pos_deltas,
@@ -59,7 +47,6 @@ class GraphSkeletonDataset(Dataset):
                     'parent_indices': parent_indices,
                     'offsets': offsets,
                     'fk_pos': fk_pos,
-                    'foot_contact': foot_contact,
                     'root_pos_absolute': root_pos
                 }
                 torch.save(data, cache_path)
@@ -75,13 +62,10 @@ class GraphSkeletonDataset(Dataset):
 
         first_data = self.cache[self.files[0]]
         self.num_joints = len(first_data['joint_names'])
+        self.joint_names = first_data['joint_names']
         self.edge_index = build_edge_index_from_parents(first_data['parent_indices'])
         self.parent_indices = first_data['parent_indices']
         self.offsets = first_data['offsets']
-        self.foot_joint_indices = get_joint_indices_by_name(
-            all_joint_names=first_data['joint_names'],
-            target_joint_names=foot_joint_names
-        )
 
         concat_root_pos_deltas = torch.cat(all_root_deltas, dim=0) # [F, 3]
         self.root_mean = concat_root_pos_deltas.mean(dim=0)            
@@ -125,8 +109,6 @@ class GraphSkeletonDataset(Dataset):
 
         fk_pos = data['fk_pos'][tgt_start : post_ctx_start]
 
-        foot_contact = data['foot_contact'][tgt_start : post_ctx_start]
-
         last_root_pos_absolute = data['root_pos_absolute'][tgt_start - 1].unsqueeze(0)
 
         return Data(
@@ -136,6 +118,5 @@ class GraphSkeletonDataset(Dataset):
             root_pos_ctx=root_ctx_norm,
             root_pos_tgt=root_tgt_norm,
             fk_pos=fk_pos,
-            foot_contact=foot_contact,
             last_root_pos_absolute=last_root_pos_absolute
         )

src/utils/rotation.py
@@ -4,18 +4,6 @@ from scipy.spatial.transform import Rotation as R
 
 
 def euler_zyx_to_rot_6d(euler_angles):
-    # Euler angles [F, J, 3] (ZYX, rad) -> 6D [F, J, 6]
-    
-    F, J, _ = euler_angles.shape
-    euler_flat = euler_angles.reshape(-1, 3) # [F * J, 3]
-    r = R.from_euler('zyx', euler_flat, degrees=False)
-    rot_mats = r.as_matrix() # [F * J, 3, 3]
-    rot_6d = rot_mats[:, :, :2].reshape(F, J, 6)
-    
-    return rot_6d
-
-
-def euler_zyx_to_rot_6d_but_correct_this_time(euler_angles):
     # Euler angles [(shape), 3] (ZYX, rad) -> 6D [(shape), 6]
     
     orig_shape = euler_angles.shape[ : -1]
@@ -57,7 +45,8 @@ def rot_6d_to_euler_zyx(rot_6d):
 
     rot_matrix = rot_matrix.detach().cpu().numpy()
 
-    euler = R.from_matrix(rot_matrix).as_euler('zyx', degrees=False)  
+    euler = R.from_matrix(rot_matrix).as_euler('xyz', degrees=False)  
+    euler = euler[:, [2, 1, 0]]
     euler = euler.reshape(*orig_shape, 3)
 
     return euler

test.py
@@ -39,9 +39,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {device}")
 
 # Evaluate function
-def evaluate(model, loader, root_loss_weight, fk_loss_weight, J, F_target, offsets, parent_indices, root_mean, root_std,
-            foot_skate_loss_weight, foot_joint_indices):
-    
+def evaluate(model, loader, root_loss_weight, fk_loss_weight, J, F_target, offsets, parent_indices, root_mean, root_std):
     model.eval()
     mse = torch.nn.MSELoss()
     mae = torch.nn.L1Loss()
@@ -50,8 +48,6 @@ def evaluate(model, loader, root_loss_weight, fk_loss_weight, J, F_target, offse
     mse_root_total = 0.0
     geo_rot_total = 0.0
     mae_fk_total = 0.0
-    total_foot_skate_loss = 0.0
-    total_smoothness_loss = 0.0
     n_batches = 0
 
     with torch.no_grad():
@@ -84,48 +80,30 @@ def evaluate(model, loader, root_loss_weight, fk_loss_weight, J, F_target, offse
                 rot_6d=rot_pred
             ) 
 
-            fk_pos_tgt_flat = batch.fk_pos.view(batch.num_graphs, -1)
-            fk_pos_pred_flat = fk_pos_pred.view(batch.num_graphs, -1)
-            loss_fk = mae(fk_pos_pred_flat, fk_pos_tgt_flat)
-
-            # Foot skating loss
-            tgt_foot_contact = batch.foot_contact.view(batch.num_graphs, F_target, -1)  # [B, F_target, n_feet]
-            foot_skate_loss = foot_skating_loss(
-                fk_pos_pred=fk_pos_pred, 
-                tgt_foot_contact=tgt_foot_contact, 
-                foot_joint_indices=foot_joint_indices
-            )
-
-            # Smoothness loss
-            smoothness_loss = compute_smoothness_loss(fk_pos_pred=fk_pos_pred)
+            fk_pos_tgt = batch.fk_pos.view(batch.num_graphs, -1)
+            fk_pos_pred = fk_pos_pred.view(batch.num_graphs, -1)
+            loss_fk = mae(fk_pos_pred, fk_pos_tgt)
 
             # Losses
-            loss = fk_loss_weight * loss_fk + root_loss_weight * loss_root_pos + loss_rot 
-            loss += foot_skate_loss_weight * foot_skate_loss
+            loss = fk_loss_weight * loss_fk + root_loss_weight * loss_root_pos + loss_rot
             total_loss += loss.item() * batch.num_graphs
             num_samples += batch.num_graphs
 
             geo_rot_total += loss_rot.item()
             mse_root_total += loss_root_pos.item()
             mae_fk_total += loss_fk.item()
-            total_foot_skate_loss += foot_skate_loss.item()
-            total_smoothness_loss += smoothness_loss.item()
             n_batches += 1
 
     avg_loss = total_loss / max(1, num_samples)
     avg_mse_root = mse_root_total / max(1, n_batches)  
     avg_geo_rot = geo_rot_total / max(1, n_batches)
     avg_mae_fk = mae_fk_total / max(1, n_batches)
-    avg_foot_skate_loss = total_foot_skate_loss / max(1, n_batches)
-    avg_smoothness_loss = total_smoothness_loss / max(1, n_batches)
 
     return {
         "total": avg_loss,
         "mse_root": avg_mse_root,
         "geo_rot": avg_geo_rot,
-        "mae_fk": avg_mae_fk,
-        "foot_skate_loss": avg_foot_skate_loss,
-        "smoothness_loss": avg_smoothness_loss
+        "mae_fk": avg_mae_fk
     }
 
 
@@ -136,10 +114,7 @@ test_dataset = GraphSkeletonDataset(
     context_len_pre=config["context_len_pre"],
     context_len_post=config["context_len_post"],
     target_len=config["target_len"],
-    step=config["step"],
-    foot_joint_names=config["foot_joint_names"],
-    foot_height_eps=config["foot_height_eps"],
-    foot_velocity_eps=config["foot_velocity_eps"]
+    step=config["step"]
 )
 print(f"Dataset ready with {len(test_dataset)} samples")
 
@@ -178,8 +153,6 @@ results = evaluate(
     loader=test_loader,
     root_loss_weight=config["root_loss_weight"],
     fk_loss_weight=config["fk_loss_weight"],
-    foot_skate_loss_weight=config["foot_skate_loss_weight"],
-    foot_joint_indices=test_dataset.foot_joint_indices.to(device),
     J=test_dataset.num_joints,
     F_target=config["target_len"],
     offsets=test_dataset.offsets.to(device),
@@ -203,5 +176,3 @@ log_str(f"Total loss - weighted sum:       {results['total']:.7f}")
 log_str(f"6D rotations geodesic loss:      {results['geo_rot']:.7f}")
 log_str(f"Root positions MSE:              {results['mse_root']:.7f}")
 log_str(f"FK positions MAE:                {results['mae_fk']:.7f}")
-log_str(f"Foot skating loss:               {results['foot_skate_loss']:.7f}")
-log_str(f"Smoothness loss (not in sum):    {results['smoothness_loss']:.7f}")

train.py
@@ -47,10 +47,7 @@ dataset = GraphSkeletonDataset(
     context_len_pre=config["context_len_pre"],
     context_len_post=config["context_len_post"],
     target_len=config["target_len"],
-    step=config["step"],
-    foot_joint_names=config["foot_joint_names"],
-    foot_height_eps=config["foot_height_eps"],
-    foot_velocity_eps=config["foot_velocity_eps"]
+    step=config["step"]
 )
 print(f"Dataset ready with {len(dataset)} samples")
 
@@ -111,22 +108,18 @@ os.makedirs(constants["model_path"], exist_ok=True)
 model_path = constants["model_path"] + filename + constants["model_suffix"]
 root_loss_weight = config["root_loss_weight"]
 fk_loss_weight = config["fk_loss_weight"]
-foot_skate_loss_weight = config["foot_skate_loss_weight"]
 patience = config["patience"]
 epochs = config["epochs"]
 F_target = config["target_len"]
 node_features = config["node_features"]
-smoothness_loss_weight = config["smoothness_loss_weight"]
-epoch_loss_switch = config["epoch_loss_switch"]
 
 J = dataset.num_joints
 root_mean = dataset.root_mean.to(device).view(1, 1, 3)
 root_std = dataset.root_std.to(device).view(1, 1, 3)
 parent_indices = dataset.parent_indices.to(device)
 offsets = dataset.offsets.to(device)
-foot_joint_indices = dataset.foot_joint_indices.to(device)
 
-best_base_val_loss = float('inf')
+best_val_loss = float('inf')
 epochs_no_improve = 0
 
 for epoch in range(1, epochs + 1):
@@ -157,7 +150,7 @@ for epoch in range(1, epochs + 1):
         rot_pred = rot_pred.view(batch.num_graphs, J, F_target, 6).permute(0, 2, 1, 3) # [B * J, F_target * 6] -> [B, F_target, J, 6]
         root_pos_pred = root_pos_pred.view(batch.num_graphs, F_target, 3)
         root_pos_pred = root_pos_pred * root_std + root_mean
-        
+
         last_root_pos_absolute = batch.last_root_pos_absolute.view(batch.num_graphs, 1, 3) # position at time target start - 1
         root_pos_pred_absolute = last_root_pos_absolute + torch.cumsum(root_pos_pred, dim=1)
 
@@ -168,30 +161,12 @@ for epoch in range(1, epochs + 1):
             rot_6d=rot_pred
         ) 
 
-        fk_pos_tgt_flat = batch.fk_pos.view(batch.num_graphs, -1)
-        fk_pos_pred_flat = fk_pos_pred.view(batch.num_graphs, -1)
-        loss_fk = mae(fk_pos_pred_flat, fk_pos_tgt_flat)
-
-        # Foot skating loss
-        if epoch < epoch_loss_switch:
-            foot_skate_loss = torch.tensor(0.0, device=device)
-        else:
-            tgt_foot_contact = batch.foot_contact.view(batch.num_graphs, F_target, -1)  # [B, F_target, n_feet]
-            foot_skate_loss = foot_skating_loss(
-                fk_pos_pred=fk_pos_pred, 
-                tgt_foot_contact=tgt_foot_contact, 
-                foot_joint_indices=foot_joint_indices
-            )
-
-        # Smoothness loss
-        if epoch >= epoch_loss_switch:
-            smoothness_loss = torch.tensor(0.0, device=device)
-        else:
-            smoothness_loss = compute_smoothness_loss(fk_pos_pred=fk_pos_pred)
+        fk_pos_tgt = batch.fk_pos.view(batch.num_graphs, -1)
+        fk_pos_pred = fk_pos_pred.view(batch.num_graphs, -1)
+        loss_fk = mae(fk_pos_pred, fk_pos_tgt)
 
         # Total loss
-        loss = loss_rot + root_loss_weight * loss_root_pos + fk_loss_weight * loss_fk 
-        loss += foot_skate_loss_weight * foot_skate_loss + smoothness_loss_weight * smoothness_loss
+        loss = loss_rot + root_loss_weight * loss_root_pos + fk_loss_weight * loss_fk
         loss.backward()
 
         optimizer.step()
@@ -205,9 +180,6 @@ for epoch in range(1, epochs + 1):
     total_rot_loss = 0.0
     total_root_loss = 0.0
     total_fk_loss = 0.0
-    total_foot_skate_loss = 0.0
-    total_smoothness_loss = 0.0
-    total_val_base_loss = 0.0
     with torch.no_grad():
         for batch in tqdm(val_loader, desc="Val", leave=False):
             batch = batch.to(device)
@@ -238,60 +210,34 @@ for epoch in range(1, epochs + 1):
                 rot_6d=rot_pred
             ) 
 
-            fk_pos_tgt_flat = batch.fk_pos.view(batch.num_graphs, -1)
-            fk_pos_pred_flat = fk_pos_pred.view(batch.num_graphs, -1)
-            loss_fk = mae(fk_pos_pred_flat, fk_pos_tgt_flat)
-
-            # Foot skating loss
-            if epoch < epoch_loss_switch:
-                foot_skate_loss = torch.tensor(0.0, device=device)
-            else:
-                tgt_foot_contact = batch.foot_contact.view(batch.num_graphs, F_target, -1)  # [B, F_target, n_feet]
-                foot_skate_loss = foot_skating_loss(
-                    fk_pos_pred=fk_pos_pred, 
-                    tgt_foot_contact=tgt_foot_contact, 
-                    foot_joint_indices=foot_joint_indices
-                )
-
-            # Smoothness loss
-            if epoch >= epoch_loss_switch:
-                smoothness_loss = torch.tensor(0.0, device=device)
-            else:
-                smoothness_loss = compute_smoothness_loss(fk_pos_pred=fk_pos_pred)
-
+            fk_pos_tgt = batch.fk_pos.view(batch.num_graphs, -1)
+            fk_pos_pred = fk_pos_pred.view(batch.num_graphs, -1)
+            loss_fk = mae(fk_pos_pred, fk_pos_tgt)
+                
             # Losses
-            base_loss = loss_rot + root_loss_weight * loss_root_pos + fk_loss_weight * loss_fk
-            loss = base_loss + foot_skate_loss_weight * foot_skate_loss + smoothness_loss_weight * smoothness_loss
-
-            total_val_base_loss += base_loss.item() * batch.num_graphs
+            loss = loss_rot + root_loss_weight * loss_root_pos + fk_loss_weight * loss_fk
             total_val_loss += loss.item() * batch.num_graphs
             total_rot_loss += loss_rot.item() * batch.num_graphs
             total_root_loss += loss_root_pos.item() * batch.num_graphs
             total_fk_loss += loss_fk.item() * batch.num_graphs
-            total_foot_skate_loss += foot_skate_loss.item() * batch.num_graphs
-            total_smoothness_loss += smoothness_loss.item() * batch.num_graphs
 
     avg_val_loss = total_val_loss / len(val_dataset)
-    avg_val_base_loss = total_val_base_loss / len(val_dataset)
-    log_str(f"Validation base loss:         {avg_val_base_loss:.7f}")
     log_str(f"Validation loss:              {avg_val_loss:.7f}")
     log_str(f"Rotations geodesic loss:      {total_rot_loss / len(val_dataset):.7f}")
     log_str(f"Root positions MSE:           {total_root_loss / len(val_dataset):.7f}")
     log_str(f"FK positions MAE:             {total_fk_loss / len(val_dataset):.7f}")
-    log_str(f"Foot skating loss:            {total_foot_skate_loss / len(val_dataset):.7f}")
-    log_str(f"Smoothness loss:              {total_smoothness_loss / len(val_dataset):.7f}")
 
-    scheduler.step(avg_val_base_loss)
+    scheduler.step(avg_val_loss)
     
-    if avg_val_base_loss < best_base_val_loss:
-        best_base_val_loss = avg_val_base_loss
+    if avg_val_loss < best_val_loss:
+        best_val_loss = avg_val_loss
         epochs_no_improve = 0
-        log_str("Base validation loss improved, saving checkpoint")
+        log_str("Validation loss improved, saving checkpoint")
         torch.save(model.state_dict(), model_path)
         log_str(f"Model saved to: {model_path}")
     else:
         epochs_no_improve += 1
-        log_str(f"No improvement in base validation loss for {epochs_no_improve} epochs")
+        log_str(f"No improvement in validation loss for {epochs_no_improve} epochs")
         if epochs_no_improve >= patience:
             log_str(f"Early stopped at epoch {epoch}")
             break