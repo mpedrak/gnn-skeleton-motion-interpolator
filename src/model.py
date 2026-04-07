934eb291588b52cca23f52803e3b3425cc1ad703
mpedrak <michalpedrak@student.agh.edu.pl>
2026-04-07 20:15:45 +0200

    GNN norm and residual connections and better embedding for MLP

--- src/model.py
+++ src/model.py
@@ -3,6 +3,7 @@ import torch.nn.functional as F
 import torch
 
 from torch_geometric.nn import GATConv
+from torch_geometric.nn.aggr import AttentionalAggregation
 
 
 class ResidualLinearBlock(nn.Module):
@@ -26,79 +27,89 @@ class SkeletalMotionInterpolator(nn.Module):
         super().__init__()
         context_len = context_len_pre + context_len_post
 
-        # GAT layers for rotations
-        graph_in_features = rot_gnn_params["num_features_in"] * context_len
-        hidden_dim = rot_gnn_params["hidden_dim"] 
-        heads = rot_gnn_params["num_heads"]
-        dropout_val = rot_gnn_params["dropout"]
-        graph_out_features = hidden_dim * heads
-        fc_rot_out_dim = rot_gnn_params["num_features_out"] * target_len
+        # GAT layers for rotations 
+        gat_hidden_dim = rot_gnn_params["hidden_dim"] 
+        heads = rot_gnn_params["num_heads"] 
+        gat_out_features = gat_hidden_dim * heads
         
         self.convs = []
         self.convs.append(
             GATConv(
-                in_channels=graph_in_features,
-                out_channels=hidden_dim, 
+                in_channels=rot_gnn_params["num_features_in"] * context_len,
+                out_channels=gat_hidden_dim, 
                 heads=heads, 
                 concat=True, 
-                dropout=dropout_val
+                dropout=rot_gnn_params["dropout"]
             )
         )
         
         for _ in range(rot_gnn_params["num_layers"] - 1): 
             self.convs.append(
                 GATConv(
-                    in_channels=hidden_dim * heads,
-                    out_channels=hidden_dim, 
+                    in_channels=gat_hidden_dim * heads,
+                    out_channels=gat_hidden_dim, 
                     heads=heads, 
                     concat=True, 
-                    dropout=dropout_val
+                    dropout=rot_gnn_params["dropout"]
                 )
             )
 
         self.convs = nn.ModuleList(self.convs)
-        self.dropout = nn.Dropout(dropout_val)
-        self.fc_rot = nn.Linear(in_features=graph_out_features, out_features=fc_rot_out_dim)
+        self.gat_norm = nn.LayerNorm(gat_out_features)
+        self.dropout = nn.Dropout(rot_gnn_params["dropout"])
+        self.fc_rot = nn.Linear(in_features=gat_out_features, out_features=rot_gnn_params["num_features_out"] * target_len)
 
         # MLP for root positions
         root_pos_in = (context_len * root_pos_mlp_params["num_features_in"]) + root_pos_mlp_params["num_features_in_from_gnn"]
         root_pos_out = target_len * root_pos_mlp_params["num_features_out"]
-        hidden_dim = root_pos_mlp_params["hidden_dim"]
-        dropout_val = root_pos_mlp_params["dropout"]
+        mlp_hidden_dim = root_pos_mlp_params["hidden_dim"]
 
-        self.reduce_graph_dim = nn.Linear(graph_out_features, root_pos_mlp_params["num_features_in_from_gnn"])
+        self.attention_net = nn.Sequential(
+            nn.Linear(gat_out_features, mlp_hidden_dim),
+            nn.LeakyReLU(),
+            nn.Linear(mlp_hidden_dim, 1)
+        )
+        self.global_pool = AttentionalAggregation(gate_nn=self.attention_net)
+
+        self.reduce_graph_dim = nn.Linear(gat_out_features, root_pos_mlp_params["num_features_in_from_gnn"])
     
         mlp_layers = []
-        mlp_layers.append(nn.Linear(in_features=root_pos_in, out_features=hidden_dim))
+        mlp_layers.append(nn.Linear(in_features=root_pos_in, out_features=mlp_hidden_dim))
 
         for _ in range(root_pos_mlp_params["num_layers"] - 2):
-            mlp_layers.append(ResidualLinearBlock(hidden_dim=hidden_dim, dropout_val=dropout_val))
+            mlp_layers.append(ResidualLinearBlock(hidden_dim=mlp_hidden_dim, dropout_val=root_pos_mlp_params["dropout"]))
 
         mlp_layers.append(nn.LeakyReLU())
-        mlp_layers.append(nn.Linear(in_features=hidden_dim, out_features=root_pos_out))
+        mlp_layers.append(nn.Linear(in_features=mlp_hidden_dim, out_features=root_pos_out))
 
         self.root_pos_mlp = nn.Sequential(*mlp_layers)
 
 
     def forward(self, data):
+        
+        if not hasattr(data, 'num_graphs'): batch_size = 1
+        else: batch_size = data.num_graphs
+
         # Rotations
         x, edge_index = data.x, data.edge_index
         for i, conv in enumerate(self.convs):
+            x_prev = x
             x = conv(x, edge_index)
+            if i != 0: x = x + x_prev # Residual connection
+
             if i != len(self.convs) - 1:
+                x = self.gat_norm(x)
                 x = F.leaky_relu(x)
-                x = self.dropout(x)
+                x = self.dropout(x)     
 
         rot_pred = self.fc_rot(x) 
 
         # Root positions
-        if not hasattr(data, 'num_graphs'): batch_size = 1
-        else: batch_size = data.num_graphs
+        if hasattr(data, 'batch') and data.batch is not None: batch = data.batch 
+        else: batch = torch.zeros(x.size(0), dtype=torch.long, device=x.device)
 
         root_pos_ctx = data.root_pos_ctx.view(batch_size, -1) 
-        J = data.num_nodes // batch_size
-        graph_out_reshaped = x.view(batch_size, J, -1) # [B, J, graph_out_features]
-        root_graph_out = graph_out_reshaped[:, 0, :]
+        root_graph_out = self.global_pool(x, batch)
         root_graph_reduced = F.relu(self.reduce_graph_dim(root_graph_out))
         root_mlp_ctx = torch.cat([root_pos_ctx, root_graph_reduced], dim=1) 
 
